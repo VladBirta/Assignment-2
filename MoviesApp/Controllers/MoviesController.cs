59733f348963fd66705fbf3334750995c5208510
Vlad Birta <vlad.birta@emanuel.ro>
2021-03-10 17:18:45 +0200

    First commit

---
+++ MoviesApp/Controllers/MoviesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MoviesApp.Data;
+using MoviesApp.Models;
+
+namespace MoviesApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        static private List<Movie> movies = new List<Movie>();
+
+        private readonly MoviesAppContext _context;
+
+        public MoviesController(MoviesAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Movies
+        public IActionResult Index()
+        {
+            return View(movies);
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = movies.Find(m => m.id == id);
+             
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("name,genre,id")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                movie.id = Guid.NewGuid();
+                movies.Add(movie);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public IActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = movies.Find(m => m.id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Guid id, [Bind("name,genre,id")] Movie movie)
+        {
+            if (id != movie.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var newMovie = movies.Find(m => m.id == id);
+                newMovie.name = movie.name;
+                newMovie.genre = movie.genre;
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public IActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = movies.Find(m => m.id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(Guid id)
+        {
+            var movie = movies.Find(m => m.id == id);
+            movies.Remove(movie);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(Guid id)
+        {
+            return movies.Any(e => e.id == id);
+        }
+    }
+}
---
+++ MoviesApp/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApp.Models
+{
+    public class Movie
+    {
+    
+        public string name { get; set; }
+        public string genre { get; set; }
+        public Guid id { get; set; }
+    }
+}
